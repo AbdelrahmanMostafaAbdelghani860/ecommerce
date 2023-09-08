f57a1383967c6a6266c7613d457ac60a26d27c3a
Abdelrahman Moustafa <abdo99792@gmail.com>
2023-09-08 22:45:38 +0300

    edit in index

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = 5000;
+app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+
 bootstrap(app, express);
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
